26caec2c57d40c11babda0221bafbf988bca82ee
Johannes Künsebeck <kuensebeck@googlemail.com>
2014-03-29 19:30:15 +0100

    Worst Inspector Warnings fixed

--- Classes/Org/Gucken/Rad/ViewHelpers/FieldsViewHelper.php
+++ Classes/Org/Gucken/Rad/ViewHelpers/FieldsViewHelper.php
@@ -2,11 +2,12 @@
 namespace Org\Gucken\Rad\ViewHelpers;
 
 use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 /**
  * @Flow\Scope("prototype")
  */
-class FieldsViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+class FieldsViewHelper extends AbstractViewHelper {
 
 	/**
 	 * 
@@ -18,7 +19,7 @@ class FieldsViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 	protected function render($object = null,$properties = array()) {
             $this->viewHelperVariableContainer->add(get_class($this), 'fieldsObject', $object);
             return 'TODO: implement '.__METHOD__;
-            return '!' .$this->renderChildren().'!'.\TYPO3\Flow\var_dump($object,'',true);
+            #return '!' .$this->renderChildren().'!'.\TYPO3\Flow\var_dump($object,'',true);
 	}
 
 }
